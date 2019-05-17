7c725da5d3028e3923ef6d84f26b6db4be3861ae
Michael Brown <Vurclash@gmail.com>
2019-05-17 15:39:32 -0600

    modified routes, updated controller

--- app/controllers/billboards_controller.rb
+++ app/controllers/billboards_controller.rb
@@ -1,4 +1,6 @@
 class BillboardsController < ApplicationController
+  before_action :set_billboard, only: [:edit, :show, :destroy, :update]
+
   def index
     @billboard = Billboards.all
   end
@@ -11,14 +13,35 @@ class BillboardsController < ApplicationController
   end
 
   def create 
+    @billboard = Billboards.new(billboard_params)
+    if @billboard.save
+      redirect_to billboards_path
+    else 
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    if @billboard.update(billboard_params)
+      redirect_to @billboard
+    else
+      render :edit
   end
 
   def destroy
+    @billboard.destroy
+    redirect_to billboards_path
+  end
+
+  private
+
+  def set_billboard
+    @billboard = Billboard.find(params[:id])
+
+  def billboard_params
+    params.require(:billboard).permit(:title)
   end
 end
